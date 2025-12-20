6f894003522e4c72558e37e466d38cf65bd10868
Phy Lieng <phylieng97@gmail.com>
2025-12-20 22:18:10 +0900

    feature/update custom drawer

--- src/components/custom_drawer/custom_drawer.tsx
+++ src/components/custom_drawer/custom_drawer.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { Drawer, IconButton, ListItemIcon, Divider, List, ListItem, ListItemText } from '@mui/material';
-import { makeStyles } from '@mui/styles';
+import {Divider, Drawer, IconButton, List} from '@mui/material';
+import {makeStyles} from '@mui/styles';
 import {drawerWidth} from "../../constants/default_constants";
 import {useDrawerToggleContext} from "../../context/DrawerOpenProvider/drawerOpenProvider.context";
 import {ChevronLeftOutlined} from "@mui/icons-material";
 import {IRouter, routes} from "../../constants/routes.constant";
 import clsx from "clsx";
-import MailIcon from "@mui/icons-material/Mail";
 import ListItemLink from "./list_item_link/list_item_link";
 
 const useStyles = makeStyles((theme: any) => ({
@@ -90,7 +89,7 @@ const CustomDrawer = () => {
             <List>
                 {
                     routes.map((route: IRouter, index: number) => {
-                            return (<div key={route.title}> <ListItemLink  route={route}/>
+                            return (<div key={route.title}><ListItemLink route={route}/>
                                     <Divider/>
                                 </div>
                             );
